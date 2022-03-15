ab3aafe9df259b668aa24d4ccf59cda56a8f711a
Rakibul Alam <51063464+rakibulalam9200@users.noreply.github.com>
2022-03-15 16:54:45 +0600

    used google fonts

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import JobsScreen from "./src/screens/JobsScreen";
 
-export default function App() {
-  return (
-    <View>
-      <JobsScreen></JobsScreen>
-    </View>
-  );
-}
+import {
+    useFonts,
+    Montserrat_200ExtraLight,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+} from '@expo-google-fonts/montserrat';
 
+export default function App() {
+
+    let [fontsLoaded] = useFonts({
+        Montserrat_300Light,
+        Montserrat_200ExtraLight,
+        Montserrat_400Regular,
+        Montserrat_500Medium,
+        Montserrat_600SemiBold,
+        Montserrat_700Bold,
+        Montserrat_800ExtraBold,
+    });
+    if (!fontsLoaded) {
+        return <AppLoading/>;
+    }else{
+        return (
+            <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+                <JobsScreen/>
+            </View>
+        );
+    }
+
+}
--- src/screens/JobsScreen.js
+++ src/screens/JobsScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
-
+import {Text, View,styleSheet} from 'react-native';
+import LeftIcon from '../../assets/svgIcon/LeftIcon';
+import Logo from "../../assets/svgIcon/Logo";
 const JobsScreen = () => {
     return (
-        <Text>
-
-        </Text>
+        <View>
+            <Text style={{fontSize:24,paddingVertical:6,fontFamily:'Montserrat_800ExtraBold'}}>This is Job Screen</Text>
+            <LeftIcon width={40} height={40}/>
+            <Logo width={115} height={32}/>
+        </View>
     );
 };
 
 export default JobsScreen;
+
+
+//fontSize:24,paddingVertical:6,fontFamily:'Montserrat_800ExtraBold'
