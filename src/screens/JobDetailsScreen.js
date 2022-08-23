b153c8db206f1c955395411e6c741c4222673a38
shaheen2013 <coredeveloper.2013@gmail.com>
2022-08-23 21:48:09 +0600

    Worked on Job Details screen animation

--- src/screens/JobDetailsScreen.js
+++ src/screens/JobDetailsScreen.js
@@ -1,9 +1,8 @@
 import { Feather, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
 import { useIsFocused } from '@react-navigation/native';
-import React, { useContext } from 'react';
-import { ActivityIndicator, Animated, Dimensions, FlatList, StatusBar, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors, Text, TouchableOpacity, View } from 'react-native-ui-lib';
+import React, { useContext, useState } from 'react';
+import { ActivityIndicator, Animated, Dimensions, FlatList, StatusBar, StyleSheet, Text } from 'react-native';
+import { Colors, TouchableOpacity, View } from 'react-native-ui-lib';
 import FilledBtn from "../components/buttons/FilledBtn";
 import JobDetailsHeader from "../components/JobDetailsComponents/JobDetailsHeader";
 import JobDetailsInfo from "../components/JobDetailsComponents/JobDetailsInfo";
@@ -11,20 +10,24 @@ import JobResponsibility from "../components/JobDetailsComponents/JobResponsibil
 import { Context as AuthContext } from "../contexts/AuthContext";
 import useSingleJob from "../hooks/useSingleJob";
 
+
 const screenWidth = Dimensions.get('window').width;
 function FocusAwareStatusBar(props) {
     const isFocused = useIsFocused();
     return isFocused ? <StatusBar {...props} /> : <StatusBar backgroundColor={Colors.white} barStyle='dark-content'/>;
 }
 
-const hSecHeight = 200;
+const hSecHeight = 270;
 
 const JobDetails = ({route,navigation}) => {
     const {slug} = route.params;
     const [singleJob,isLoading] = useSingleJob(slug);
     const {state:{token}} = useContext(AuthContext);
-    const offset = new Animated.Value(0);
     const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+    const animatedValue = new Animated.Value(0);
+    const [fullHeader,setFullHeader] = useState(true);
+    const [shortHeader,setShortHeader] = useState(false);
+
 
     if (!singleJob) {
         return null;
@@ -50,29 +53,17 @@ const JobDetails = ({route,navigation}) => {
         }else{
             navigation.navigate('Apply',{title:singleJob?.title,job_slug:slug});
         }
-    }
+    } 
 
-    const headerHeight = offset.interpolate({
-        inputRange: [0, hSecHeight + SafeAreaView.top],
-        outputRange: [hSecHeight + SafeAreaView.top, SafeAreaView.top + 60],
-        extrapolate: "clamp",
-      });
-    
-      const sliderHeight = offset.interpolate({
-        inputRange: [0, hSecHeight + SafeAreaView.top],
-        outputRange: [hSecHeight + SafeAreaView.top - 26, 1],
-        extrapolate: "clamp",
-      });
-    
-      const opacityForSlider = offset.interpolate({
-        inputRange: [hSecHeight, hSecHeight + SafeAreaView.top],
-        outputRange: [1, 0],
-        extrapolate: "clamp",
-      });
+      let translateY = animatedValue.interpolate({
+        inputRange: [0, 310],
+        outputRange: [0, -110],
+        extrapolate: 'clamp',
+    });
     
 
     return (
-        <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1}}>
             <FocusAwareStatusBar barStyle={Colors.white} backgroundColor={Colors.blue}/>
             <JobDetailsHeader name={'Jobs Details'} navigation={navigation}/>
             {isLoading ?
@@ -81,29 +72,29 @@ const JobDetails = ({route,navigation}) => {
                 </View>)
                 :
                     (
-                        <View style={{flex:1}} forceInset={{ top: "always" }}>
-                            {singleJob?.job_summery &&
-                            <Animated.View  style={{
-                                    backgroundColor: Colors.blue,
-                                    paddingHorizontal:16,
-                                    paddingBottom:12,
-                                    position:'absolute',
-                                    top: 0,
-                                    left: 0,
-                                    right: 0,
-                                    height: 180,
-                                    zIndex:1,
-                                }}>
-                                <Animated.View 
-                                style={{
-                                    opacity: opacityForSlider,
-                                    overflow: "hidden",
-                                    height: sliderHeight,
-                                }}
+                        <View style={{flex:1}}>
+                            {
+                                singleJob?.job_summery && 
+                                <View>
+                                    <View style={{
+                                        backgroundColor: Colors.blue,
+                                        paddingHorizontal:16,
+                                        paddingBottom:12,
+                                         
+                                        },[shortHeader && {zIndex:99}]}>
+                                        <Text style={{color:'white',fontFamily:"Montserrat_600SemiBold",marginVertical:8,fontSize:15}}>{singleJob?.title}</Text>
+                                        <JobDetailsInfo icon={"users"} title={"Experience:"}
+                                                            text={singleJob?.job_summery?.experience}
+                                                            IconLib={Feather}/>
+                                        <JobDetailsInfo icon={"currency-usd-circle-outline"} title={"Salary:"}
+                                                    text={singleJob?.job_summery?.salary_range}
+                                                    IconLib={MaterialCommunityIcons}/>
+                                    </View>
+                                
+                                  {fullHeader && <Animated.View style={[styles.headerWrapper, {transform: [{translateY}]}]}>
+                                    <View style={{backgroundColor: Colors.blue, paddingHorizontal:16, paddingBottom:12,}}>
+                                         <Text style={{color:'white',fontFamily:"Montserrat_600SemiBold",marginVertical:8,fontSize:15}}>{singleJob?.title}</Text>
                                     
-                               >
-                                    <Text h4 marginV-8 white>{singleJob?.title}</Text>
-                                    <View>
                                         <View>
                                             <JobDetailsInfo icon={"calendar"} title={"Published:"} text={singleJob?.updated_at}
                                                             IconLib={Feather}/>
@@ -118,31 +109,48 @@ const JobDetails = ({route,navigation}) => {
                                                             text={singleJob?.job_summery?.experience}
                                                             IconLib={Feather}/>
                                         </View>
-
-                                    </View>
-                                    <JobDetailsInfo icon={"currency-usd-circle-outline"} title={"Salary:"}
+                                        <JobDetailsInfo icon={"currency-usd-circle-outline"} title={"Salary:"}
                                                     text={singleJob?.job_summery?.salary_range}
                                                     IconLib={MaterialCommunityIcons}/>
-                                    <JobDetailsInfo icon={"location-pin"} title={"Location:"}
+                                        <JobDetailsInfo icon={"location-pin"} title={"Location:"}
                                                 text={"Ring Road, Mohammadpur, House 18/5, Floor 2nd, Dhaka, 1207"}
                                                 IconLib={SimpleLineIcons}/>
-                                </Animated.View>
-                            </Animated.View>
-                             
+
+                                    </View>
+                                    
+                                    </Animated.View>}
+                                </View>
                             }
-                        {/*Customized Flatlist instead of Scrollview because flatlist and scrollview cann't work nested together */}
                         {
                             <AnimatedFlatList
-                             scrollEventThrottle={16}
+                                showsVerticalScrollIndicator={false}
+                                scrollEventThrottle={16}
                                 data={[{}]}
                                 keyExtractor={() => null}
-                                onScroll={
-                                    Animated.event(
-                                    [{ nativeEvent: { contentOffset: { y: offset } } }],
-                                    { useNativeDriver: false }
-                                    )}
+                                onScroll={Animated.event(
+                                    [{nativeEvent: {contentOffset: {y: animatedValue}}}],
+                                    { 
+                                        useNativeDriver: true,
+                                        listener: (event) => {
+                                             const offsetY = event.nativeEvent.contentOffset.y;
+                                            if(offsetY >= 128){
+                                                console.log(offsetY,'offsety...')
+                                                 //setFullHeader(false);
+                                                 //setShortHeader(true);
+                                            }else{
+                                                console.log(offsetY,'less than 135 offsety...')
+                                               // setFullHeader(true);
+                                              //  setShortHeader(false);
+                                                
+                                            }
+                                             
+                                            // do something special
+                                        },
+                                    },
+                                   
+                                )}
                                 renderItem={() => <>{singleJob?.job_contexts &&
-                                        <View marginB-60>
+                                        <View marginB-60 marginT-150>
                                             <View marginT-20 paddingR-16>
                                                 <JobResponsibility points={createPoint(singleJob?.job_contexts[3]?.description)}
                                                                    title={"Job Responsibilites"}
@@ -166,7 +174,7 @@ const JobDetails = ({route,navigation}) => {
 
                                         </View>}</>}
                             />
-
+                            
                         }
 
                             {singleJob?.job_summery &&
@@ -174,15 +182,13 @@ const JobDetails = ({route,navigation}) => {
                                         <TouchableOpacity marginB-16  onPress={navigateApply}>
                                                 <FilledBtn title={"Apply Now"}/>
                                         </TouchableOpacity>
-
-
                                     </View>
 
 
                             }
                     </View>)
                 }
-        </SafeAreaView>
+        </View>
     );
 };
 
@@ -190,14 +196,30 @@ export default JobDetails;
 
 const styles = StyleSheet.create({
     applyBtnStyle:{
-        marginTop:20,
+        marginTop:40,
         position: 'absolute',
         alignSelf:'center',
         width: screenWidth - 32,
-        top: '90%',
+        top: '88%',
         backgroundColor:'white',
         borderTopEndRadius:16,
         borderTopLeftRadius: 16,
+    },
+    headerWrapper: {
+        backgroundColor: Colors.blue,
+		position: 'absolute',
+		height: 240,
+		left: 0,
+		right: 0,
+        zIndex:1,
+	},
 
+    headerTitleText:{
+        fontSize: 15, 
+        color:'white',
+        marginVertical:8,
+        fontWeight:'bold',
+        fontFamily:'Montserrat_600SemiBold',
     }
+    
 })
